7ea8b7cba271424fb420560cd1e8b7fd8cc34569
amoswang <zhehan0110@gmail.com>
2024-07-14 10:03:38 +0800

    fix dap keymap

--- lua/plugins/lsp-dap.lua
+++ lua/plugins/lsp-dap.lua
@@ -1,11 +1,12 @@
 return {
   'mfussenegger/nvim-dap',
   'rcarriga/nvim-dap-ui',
-  'theHamsta/nvim-dap-virtual-text',
   dependencies = {
     'mfussenegger/nvim-dap',
     'nvim-neotest/nvim-nio',
   },
+  'nvim-neotest/nvim-nio',
+  'theHamsta/nvim-dap-virtual-text',
 
   config = function()
     local dap, dapui = require 'dap', require 'dapui'
