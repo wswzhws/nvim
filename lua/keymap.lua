3a104cc60cd37369c631820c09511275469f0635
zhehan <zhehan0110@gmail.com>
2025-03-19 18:36:21 +0800

    add <C-a> keymap to :q!

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -10,7 +10,8 @@ keymap.set('n', 'k', [[v:count?'k':'gk']], { noremap = true, expr = true })
 keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
 keymap.set('i', 'jk', '<esc>', { noremap = true })
 
-keymap.set('n', '<C-q>', '<cmd>wq<cr>')
+keymap.set('n', '<C-a>', '<cmd>q!<cr>')
+keymap.set('n', '<leader>q', '<cmd>wq<cr>')
 
 keymap.set('n', 'H', '<C-w>h')
 keymap.set('n', 'J', '<C-w>j')
--- lua/plugins/p-project.lua
+++ lua/plugins/p-project.lua
@@ -9,6 +9,7 @@ return {
     },
     last_session_on_startup = false,
   },
+
   init = function()
     -- enable saving the state of plugins in the session
     vim.opt.sessionoptions:append 'globals' -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
